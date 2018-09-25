77c82c43d90d71f37495bb06bd9757b0b46b4bbe
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-09-25 22:15:00 +0800

    draw peaks with tags

---
+++ drawMZWithTag.m
@@ -0,0 +1,10 @@
+function drawMZWithTag( ha,mz,intens,num )
+    bar(ha,mz,intens,500);
+    ha.NextPlot = 'add';
+    [~,Is] = sort(intens,'descend');
+    for m = 1:min([num,length(mz)])
+        text(ha,mz(Is(m)),intens(Is(m)),sprintf('%.4f',mz(Is(m))),'FontSize',8);
+    end
+    ha.NextPlot = 'replace';
+end
+

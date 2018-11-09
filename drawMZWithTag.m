a5b7de8a8401f53ea9499d007981a7c1477e4b8c
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-11-09 09:28:58 +0800

    control absolute bar width

--- drawMZWithTag.m
+++ drawMZWithTag.m
@@ -1,6 +1,10 @@
 function drawMZWithTag( ha,mz,intens,num )
-    bar(ha,mz,intens,100);
+    TARGET_WIDTH = 0.2;
+    minDis = min(abs(mz(2:end)-mz(1:(end-1))));
+    factor = TARGET_WIDTH/minDis;
+    bar(ha,mz,intens,factor);
     ha.NextPlot = 'add';
+    ha.XLim = [min(mz),max(mz)];
     [~,Is] = sort(intens,'descend');
     for m = 1:min([num,length(mz)])
         text(ha,mz(Is(m)),intens(Is(m)),sprintf('%.4f',mz(Is(m))),'FontSize',8);

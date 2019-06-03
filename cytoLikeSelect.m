d981c04680140110d8acd5ebf422298cd37f88ea
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2019-06-03 17:11:15 +0800

    add small number to prevent -inf

--- cytoLikeSelect.m
+++ cytoLikeSelect.m
@@ -3,12 +3,13 @@ function [I,t,isIn] = cytoLikeSelect(setObj,r1,r2,isShow)
         isShow = 0;
     end
     t = cell2mat(setObj.getFieldByName('sampleTime'));
-    c1 = setObj.getMZRange(r1);
-    c2 = setObj.getMZRange(r2);
-    C1 = log10(c1); C2 = log10(c2);
+    c1 = setObj.getMZRange(r1)+10;
+    c2 = setObj.getMZRange(r2)+10;
+    C1 = log10(c1(:)); C2 = log10(c2(:));
     
     hf = figure;
-    scatter(C1,C2,15,'filled')
+    [f,~] = ksdensity([C1,C2],[C1,C2]);
+    scatter(C1,C2,10,f,'filled');
     a = impoly;
     pXpY = a.getPosition();
     isIn = inpolygon(C1,C2,pXpY(:,1),pXpY(:,2));

b47ee612c1360c26b1a133030ef52b306962d955
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2019-06-03 16:51:31 +0800

    fix bug

--- getFracPlace.m
+++ getFracPlace.m
@@ -1,5 +1,5 @@
 function [ res ] = getFracPlace( r )
-    res = find(mod(r*power(10,0:20),1)==0);
+    res = find(abs(r*power(10,0:20) - round(r*power(10,0:20)))<1e-9);
     res = res(1)-1;  
 end
 
